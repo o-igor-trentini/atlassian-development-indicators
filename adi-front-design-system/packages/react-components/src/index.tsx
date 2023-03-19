fc5a7f59125f44187b17bcd5eaedd7157c55a128
Igor Trentini <igortrentini.2004@gmail.com>
2023-03-19 15:13:25 -0300

    fix: Export configProvider

--- adi-front-design-system/packages/react-components/src/index.tsx
+++ adi-front-design-system/packages/react-components/src/index.tsx
@@ -1,3 +1,6 @@
+export { ConfigProvider } from './ConfigProvider';
+export type { ConfigProviderProps } from './ConfigProvider';
+
 export { Button } from './Button';
 export type { ButtonProps } from './Button';
 
