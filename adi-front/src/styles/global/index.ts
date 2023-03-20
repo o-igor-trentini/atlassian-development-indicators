89c1ac2eddebce48564935d74e0308ef192f22d6
Igor Trentini <igortrentini.2004@gmail.com>
2023-03-20 16:57:36 -0300

    refactor: Removido estlizização do estilo global

--- adi-front/src/styles/global/index.ts
+++ adi-front/src/styles/global/index.ts
@@ -1,12 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  
   // Marge para o botão detro de um formulário
   // devio a ele não ficar centralizado com os inputs
   .ant-form-item-control-input-content {
