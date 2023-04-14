fc6ba09456af53f244c0ba3f089d22daba4a42ed
Igor Trentini <igortrentini.2004@gmail.com>
2023-04-14 15:24:15 -0300

    fix: Tradução da lib antd

--- adi-front-design-system/packages/react-components/src/ConfigProvider/index.tsx
+++ adi-front-design-system/packages/react-components/src/ConfigProvider/index.tsx
@@ -1,21 +1,29 @@
 import type { FC, ReactNode } from 'react';
 import { ConfigProvider as AntdConfigProvider, theme as AntdTheme } from 'antd';
 import type { ThemeConfig } from 'antd';
-import ptBRLocale from 'antd/lib/locale/pt_BR';
-import enLocale from 'antd/lib/locale/en_GB';
+import ptBRLocale from 'antd/es/locale/pt_BR';
+import enLocale from 'antd/es/locale/en_GB';
+import { Locale } from 'antd/es/locale';
 
 export const AppTheme = AntdTheme;
 export type ThemeAlgorithm = typeof AntdTheme.defaultAlgorithm;
 
+export type LocaleVariant = 'ptBR' | 'en';
+
 export interface ConfigProviderProps {
     children: ReactNode;
     theme: ThemeConfig;
-    locale: 'ptBR' | 'en';
+    locale: LocaleVariant;
 }
 
 export const ConfigProvider: FC<ConfigProviderProps> = ({ children, theme, locale = 'ptBR' }): JSX.Element => {
+    const languages: Record<LocaleVariant, Locale> = {
+        ptBR: ptBRLocale,
+        en: enLocale,
+    };
+
     return (
-        <AntdConfigProvider theme={theme} locale={locale === 'ptBR' ? ptBRLocale : enLocale}>
+        <AntdConfigProvider theme={theme} locale={languages[locale]}>
             {children}
         </AntdConfigProvider>
     );
