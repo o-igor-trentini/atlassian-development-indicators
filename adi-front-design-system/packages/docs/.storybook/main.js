3bef3712e7cfba7675913aba46751c7723196843
Igor Trentini <igortrentini.2004@gmail.com>
2023-04-20 16:11:04 -0300

    feat: Atualizado storybook

--- adi-front-design-system/packages/docs/.storybook/main.js
+++ adi-front-design-system/packages/docs/.storybook/main.js
@@ -1,25 +1,26 @@
 module.exports = {
-  stories: [
-    "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
-  },
-  features: {
-    storyStoreV7: true,
-  },
-  // viteFinal: (config, { configType }) => {
-  //   // TODO: Configurar URL para produção
-  //   if (configType === "PRODUCTION") config.base = "/{URL}/";
-  //
-  //   return config;
-  // },
+    stories: ['../src/pages/**/*.stories.mdx', '../src/stories/**/*.stories.@(ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-a11y',
+        '@storybook/addon-mdx-gfm',
+    ],
+    framework: {
+        name: '@storybook/react-vite',
+        options: {},
+    },
+    features: {
+        storyStoreV7: true,
+    },
+    // viteFinal: (config, { configType }) => {
+    //   // TODO: Configurar URL para produção
+    //   if (configType === "PRODUCTION") config.base = "/{URL}/";
+    //
+    //   return config;
+    // },
+    docs: {
+        autodocs: true,
+    },
 };
